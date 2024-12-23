c38f6572a08070a37434ece97c73d9df64303956
Rhuan Guilherme <rhuan23032004@gmail.com>
2024-12-23 10:05:32 -0300

    Começando o projeto ja com a parte de autenticação pronta

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import fastify from 'fastify';
+import { env } from './env';
+import { UserRoutes } from '@/http/controllers/users/router';
+import fastifyJwt from '@fastify/jwt';
+import fastifyCors from '@fastify/cors';
+
+export const app = fastify();
+
+app.register(fastifyCors, {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+});
+
+app.register(fastifyJwt, { secret: env.JWT_SECRET });
+
+app.register(UserRoutes);
